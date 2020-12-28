5227e8d72c69cf760bdfbb1cfdf0ba5782570574
NotSwaraj <70449885+NotSwaraj@users.noreply.github.com>
2020-12-28 16:47:32 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import random, string
+import requests
+c = open("keywords.txt", "r")
+keywords = c.readline()
+num=input('Threads : ')
+f=open("pastes.txt","w", encoding='utf-8')
+print("Generating and checking")
+for n in range(int(num)):
+   y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(8))
+   f.write("https://pastebin.com/raw/") 
+   f.write(y)
+   f.write("\n")     
+f.close()
+k=open("combo.txt","w", encoding='utf-8')
+with open("pastes.txt") as f:
+    for line in f:
+        acc = line.strip("\n")
+        url = acc
+        s = requests.session()
+        response = s.get(url)
+        if f"{keywords}" in response.text:
+          print("Found a valid paste! " + url)
+          k.write(url)
